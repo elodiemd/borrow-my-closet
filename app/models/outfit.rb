43a59fe6f721dea625ff135894dd6b00dd05bf76
MelanieOlivier <melanie.olivier13@gmail.com>
2021-08-24 17:29:00 +0200

    index not finished

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -2,7 +2,7 @@ class Outfit < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
 
-  validates :name, :description, :location, :price_per_day, presence: true
+  validates :name, :description, :location, :price_per_day, :photos, presence: true
   validates :price_per_day, numericality: { greater_than: 0, lower_than: 10000 }
   validates :name, length: { minimum: 3 }
   validates :location, length: { minimum: 10 }
